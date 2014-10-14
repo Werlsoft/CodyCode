a300e96034a41b57140312f2d368a6e2901735b9
William Young <hddfilestorage@gmail.com>
2014-10-14 11:16:50 -0500

    More documentation

--- CodyCode/src/enums/DisplayType.java
+++ CodyCode/src/enums/DisplayType.java
@@ -1,8 +1,20 @@
 package enums;
 
 public enum DisplayType {
+	/**
+	 * Used for full length names.
+	 */
 FULL,		//Full long words
+	/**
+	 * Used for the shortest most readable name. Normally best.
+	 */
 CONCISE,	//Best fit for most cases
+	/**
+	 * 4 digit short codes. Best for consistent output.
+	 */
 SHORT,		//4 letter short codes
+	/**
+	 * No changes to input.
+	 */
 UNMODIFIED 	//Will not change anything
 }
--- CodyCode/src/util/Logger.java
+++ CodyCode/src/util/Logger.java
@@ -9,7 +9,7 @@ public interface Logger {
 	 * 
 	 * @param message The information to display
 	 * 
-	 * <p>Used to send Strings to all outputs connected to the {@link Logger} this method is run on.</p>
+	 * <p>Used to send Strings to all outputs connected to the {@link Logger} this method is run on. This method uses the Default level set using the setDefaultLevel method.</p>
 	 * <p> For example <br/>
 	 * '<code>new ConsoleLogger().log("Hello World",LoggingLevel.INFO);</code>'<br/>
 	 * would print '<code>TimeStamp-INFO: Hello World</code>' to the console ({@link ConsoleLogger} attaches to System.out automatically)
@@ -28,10 +28,57 @@ public interface Logger {
 	 * 
 	 */
 	void log(Object message,LoggingLevel level);
+	/**
+	 * 
+	 * @param format
+	 * 
+	 * <p>Used to format the output of the {@link Logger} to all outputs.</p><br/>
+	 * <ul><h5>These codes are replaced with the listed attribute</h5>
+	 * 	<li>'<code>%1$s</code>' - TimeStamp</li>
+	 * 	<li>'<code>%2$s</code>' - LoggingLevel</li>
+	 * 	<li>'<code>%3$s</code>' - Message</li>
+	 * </ul>
+	 */
 	void setFormat(String format);
+	/**
+	 * 
+	 * @return
+	 * 
+	 * <p>Returns a string representing the format used for logging.</p>
+	 */
 	String getFormat();
+	/**
+	 * 
+	 * @param level
+	 * 
+	 * <p>Sets the default level as used in the log(Object message) method</p>
+	 */
 	void setDefaultLevel(LoggingLevel level);
+	/**
+	 * 
+	 * @return
+	 * 
+	 * <p>Returns the default level as used in the log(Object message) method.</p>
+	 * 
+	 */
 	LoggingLevel getDefaultLevel();
+	/**
+	 * 
+	 * @param displayType
+	 * 
+	 * <p>Set the display type. This is used for automatic formatting of levels to create a consistent appearance regardless of the specific LoggingLevel used.</p>
+	 * <ul><h5>Effects</h5>
+	 * <li>FULL - will use the longest most explanatory name possible.</li>
+	 * <li>SHORT - will use only 4 digit short codes in the output. Good for a more constant appearance.</li>
+	 * <li>CONCISE - will use the shortest readable name possible. Normally the best choice.</li>
+	 * <li>UNMODIFIED - will use exactly the same level name as put in. Only use if you must need to use equivalent names for different purposes.</li>
+	 */
 	void setDisplayType(DisplayType displayType);
+	/**
+	 * 
+	 * @return
+	 * 
+	 * <p>Returns the currently active display type.</p>
+	 */
 	DisplayType getDisplayType();
 }
