3cb8bfbf5280b2c6c05d91e0c1400ca4b31c13c2
William Young <hddfilestorage@gmail.com>
2014-10-13 21:30:10 -0500

    Most important JavaDocs put in place
    Includes references to the usage of ConsoleLogger as a plus!

--- CodyCode/src/util/Logger.java
+++ CodyCode/src/util/Logger.java
@@ -5,7 +5,28 @@ import enums.LoggingLevel;
 
 public interface Logger {
 	
+	/**
+	 * 
+	 * @param message The information to display
+	 * 
+	 * <p>Used to send Strings to all outputs connected to the {@link Logger} this method is run on.</p>
+	 * <p> For example <br/>
+	 * '<code>new ConsoleLogger().log("Hello World",LoggingLevel.INFO);</code>'<br/>
+	 * would print '<code>TimeStamp-INFO: Hello World</code>' to the console ({@link ConsoleLogger} attaches to System.out automatically)
+	 * 
+	 */
 	void log(Object message);
+	/**
+	 * 
+	 * @param message The information to display
+	 * @param level The level to display when logging message
+	 * 
+	 * <p>Used to send Strings to all outputs connected to the {@link Logger} this method is run on.</p>
+	 * <p> For example <br/>
+	 * '<code>new ConsoleLogger().log("Hello World",LoggingLevel.INFO);</code>'<br/>
+	 * would print '<code>TimeStamp-INFO: Hello World</code>' to the console ({@link ConsoleLogger} attaches to System.out automatically)
+	 * 
+	 */
 	void log(Object message,LoggingLevel level);
 	void setFormat(String format);
 	String getFormat();
