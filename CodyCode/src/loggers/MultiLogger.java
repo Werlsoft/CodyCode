bdce91fd66217ea1d480b70eb0cee8a7fd2f4045
William Young <hddfilestorage@gmail.com>
2014-10-17 15:44:37 -0500

    Fixed multilogger for generic types

--- CodyCode/src/loggers/MultiLogger.java
+++ CodyCode/src/loggers/MultiLogger.java
@@ -1,48 +1,49 @@
 package loggers;
 
+import interfaces.Logger;
 import abstracts.BaseLogger;
 import enums.DisplayType;
 import enums.LoggingLevel;
 
 public class MultiLogger extends BaseLogger {
 	
-	protected final BaseLogger[] loggers;
+	protected final Logger[] loggers;
 	
 	
-	public MultiLogger(BaseLogger... loggers){
+	public MultiLogger(Logger... loggers){
 		this.loggers = loggers;
 	}
 
 	@Override
 	public void log(Object message, LoggingLevel level) {
-		for(BaseLogger l:loggers)
+		for(Logger l:loggers)
 			l.log(message, level);
 	}
 
 	@Override
 	public void charLog(char c) {
-		for(BaseLogger l:loggers)
+		for(Logger l:loggers)
 			l.charLog(c);
 	}
 
 	@Override
 	public void setFormat(String format) {
 		super.setFormat(format);
-		for(BaseLogger l:loggers)
+		for(Logger l:loggers)
 			l.setFormat(this.format);
 	}
 
 	@Override
 	public void setDefaultLevel(LoggingLevel level) {
 		super.setDefaultLevel(level);
-		for(BaseLogger l:loggers)
+		for(Logger l:loggers)
 			l.setDefaultLevel(this.defaultLevel);
 	}
 
 	@Override
 	public void setDisplayType(DisplayType displayType) {
 		super.setDisplayType(displayType);
-		for(BaseLogger l:loggers)
+		for(Logger l:loggers)
 			l.setDisplayType(this.displayType);
 	}
 
