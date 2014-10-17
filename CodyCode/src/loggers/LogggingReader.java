3bb275c32fc14f7ee8af514538713756ac9f16d7
William Young <hddfilestorage@gmail.com>
2014-10-17 09:17:28 -0500

    Cleaning

--- CodyCode/src/loggers/LogggingReader.java
+++ CodyCode/src/loggers/LogggingReader.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import enums.DisplayType;
-import enums.LoggingLevel;
-
 
 public class LogggingReader extends Reader{
 
